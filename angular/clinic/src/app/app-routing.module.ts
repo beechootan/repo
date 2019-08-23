ccd675f914b66d2e3e596de62429a594d8d93648
gurudevi06 <gurudevi_v@dell.com>
2019-08-23 09:24:27 +0800

    MergedWithTX

--- angular/clinic/src/app/app-routing.module.ts
+++ angular/clinic/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: 'appointment', component: AppointmentComponent },
   { path: 'home/:employeeId', component: HomeComponent },
-  { path: 'login',component: LoginComponent }
+  { path: '',component: LoginComponent }
   // { path: 'home', component: HomeComponent },
   // { path: '',component: LoginComponent }
 ]
